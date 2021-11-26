732626da183971969fc7a15e0fb13774bc7179ad
AustinMaZW <auzzyma@gmail.com>
2021-11-26 17:10:09 +0800

    Added player movements and basic combat UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2f;
+
+    [SerializeField] Rigidbody2D playerRigidBody;
+    [SerializeField] Animator playerAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement) * moveSpeed;
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if(horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+        }
+    }
+}
